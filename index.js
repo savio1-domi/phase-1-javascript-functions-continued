4072357466a5286e90baf211517c9928becb3762
git-savio1.domi <techsavvydominic3@gmail.com>
2022-11-26 11:16:25 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+
+}
+
+let mondayWork = function(activity = ("go to the office, work from home")){
+    return `This Monday, I will ${activity}.`
+    
+}
+
+
+let wrapAdjective = function(style = "*"){
+    return function(adjective = "special"){
+        return `You are ${style}${adjective}${style}!`;
+    }
+}
