654bc8de7a487eee9df7b0acdadc1fd84fe36b65
git-savio1.domi <techsavvydominic3@gmail.com>
2023-03-13 18:11:34 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 // code your solution here
-function saturdayFun(activity = "roller-skate"){
-    return `This Saturday, I want to ${activity}!`;
 
+/*uses the default activity 'roller-skate' 
+when no arguments are passed
+✓ permits the default activity to be overriden 
+*/
+function saturdayFun(skate = 'roller-skate'){
+    return `This Saturday, I want to ${skate}!`
 }
+saturdayFun()
 
-let mondayWork = function(activity = ("go to the office, work from home")){
-    return `This Monday, I will ${activity}.`
-    
+/*uses the default activity 'go to the office' 
+when no arguments are passed
+✓ permits the default activity to be overriden */
+function mondayWork(officeWork = 'go to the office'){
+    return `This Monday, I will ${officeWork}.`
 }
+mondayWork()
 
 
-let wrapAdjective = function(style = "*"){
-    return function(adjective = "special"){
-        return `You are ${style}${adjective}${style}!`;
+function wrapAdjective(wrapper = "*"){
+    return function (msg = "a hard worker"){
+        return `You are ${wrapper}${msg}${wrapper}!`;
     }
 }
+console.log(wrapAdjective('||'));
